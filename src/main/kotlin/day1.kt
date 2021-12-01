4ec24798e1a991243fdde9b1099bfe52bf036a61
christopher schicho <christopher.schicho@gmail.com>
2021-12-01 20:01:05 +0100

    day 1

---
+++ src/main/kotlin/day1.kt
@@ -0,0 +1,28 @@
+import java.io.File
+
+fun task1() {
+    print(
+        File("day1/input1.txt")
+            .readLines().map { it.toInt() }
+            .windowed(2, 1)
+            .count { it.first() < it.last() }
+    )
+}
+
+fun task2() {
+    print(
+        File("day1/input1.txt")
+            .readLines()
+            .map { it.toInt() }
+            .windowed(4, 1)
+            .count { it.subList(0, 3).sum() < it.subList(1, 4).sum() }
+    )
+}
+
+fun main() {
+    println("### TASK 1 ###")
+    task1()
+
+    println("### TASK 2 ###")
+    task2()
+}
