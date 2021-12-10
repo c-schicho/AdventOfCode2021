316210e2bfbc1c5063907e00488f68307dafc4d8
christopher schicho <christopher.schicho@gmail.com>
2021-12-10 20:21:46 +0100

    day 10

---
+++ src/main/kotlin/day10.kt
@@ -0,0 +1,98 @@
+import Chunks.Companion.calculateCorruptError
+import Chunks.Companion.calculateMissingError
+import java.io.File
+import java.util.ArrayDeque
+
+class Chunks(
+    private val chunks: List<List<Char>>
+) {
+    fun findFirstCorruptChars(): List<Char> {
+        return chunks.map { validateChunkList(it) }
+            .filter { it.isCorrupt }
+            .flatMap { it.elements }
+    }
+
+    fun findMissingChars(): List<List<Char>> {
+        return chunks.map { validateChunkList(it) }
+            .filter { !it.isCorrupt }
+            .map { it.elements }
+    }
+
+    private fun validateChunkList(chunkList: List<Char>): ValidationResult {
+        val openingElements = ArrayDeque<Char>(chunkList.size)
+        for (element in chunkList) {
+            if (element in OPENING_TO_CLOSING.keys) {
+                openingElements.push(element)
+            } else {
+                if (openingElements.pop() != CLOSING_TO_OPENING[element]) {
+                    return ValidationResult(true, listOf(element))
+                }
+            }
+        }
+        return ValidationResult(false, openingElements.map { OPENING_TO_CLOSING[it]!! }.toList())
+    }
+
+    companion object {
+        private val CORRUPT_ERROR = mapOf(
+            ')' to 3,
+            ']' to 57,
+            '}' to 1197,
+            '>' to 25137
+        )
+
+        private val MISSING_ERROR = mapOf(
+            ')' to 1,
+            ']' to 2,
+            '}' to 3,
+            '>' to 4
+        )
+
+        private val OPENING_TO_CLOSING = mapOf(
+            '(' to ')',
+            '[' to ']',
+            '{' to '}',
+            '<' to '>'
+        )
+
+        private val CLOSING_TO_OPENING = OPENING_TO_CLOSING.map { it.value to it.key }.toMap()
+
+        fun List<Char>.calculateCorruptError(): Int {
+            return this.sumOf { CORRUPT_ERROR[it]!! }
+        }
+
+        fun List<Char>.calculateMissingError(): Long {
+            var error = 0L
+            this.forEach { error = error * 5 + MISSING_ERROR[it]!! }
+            return error
+        }
+
+        fun readChunksFromFile(path: String = "data/day10/input1.txt"): Chunks {
+            return Chunks(File(path).readLines().map { it.map { it } })
+        }
+    }
+
+    private data class ValidationResult(val isCorrupt: Boolean, val elements: List<Char>)
+}
+
+private fun List<Long>.median(): Long {
+    return this.sorted()[this.size / 2]
+}
+
+private fun task1(): Int {
+    return Chunks.readChunksFromFile()
+        .findFirstCorruptChars()
+        .calculateCorruptError()
+}
+
+private fun task2(): Long {
+    return Chunks.readChunksFromFile()
+        .findMissingChars()
+        .map { it.calculateMissingError() }
+        .median()
+}
+
+fun main() {
+    println("### TASK 1 ###\n${task1()}")
+
+    println("### TASK 2 ###\n${task2()}")
+}
